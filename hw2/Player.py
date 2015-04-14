9be7d974b96de41f0982cc0e1296313acaea3e63
rivas2016 <ricardo.rivas.preuss12@gmail.com>
2015-04-14 12:43:09 -0500

    hw2

---
+++ hw2/Player.py
@@ -0,0 +1,278 @@
+# File: Player.py
+# Author(s) names AND netid's:
+# Date: 
+# Defines a simple artificially intelligent player agent
+# You will define the alpha-beta pruning search algorithm
+# You will also define the score function in the MancalaPlayer class,
+# a subclass of the Player class.
+
+
+from random import *
+from decimal import *
+from copy import *
+from MancalaBoard import *
+
+# a constant
+INFINITY = 1.0e400
+
+class Player:
+    """ A basic AI (or human) player """
+    HUMAN = 0
+    RANDOM = 1
+    MINIMAX = 2
+    ABPRUNE = 3
+    CUSTOM = 4
+    
+    def __init__(self, playerNum, playerType, ply=0):
+        """Initialize a Player with a playerNum (1 or 2), playerType (one of
+        the constants such as HUMAN), and a ply (default is 0)."""
+        self.num = playerNum
+        self.opp = 2 - playerNum + 1
+        self.type = playerType
+        self.ply = ply
+
+    def __repr__(self):
+        """Returns a string representation of the Player."""
+        return str(self.num)
+        
+    def minimaxMove(self, board, ply):
+        """ Choose the best minimax move.  Returns (score, move) """
+        move = -1
+        score = -INFINITY
+        turn = self
+        for m in board.legalMoves(self):
+            #for each legal move
+            if ply == 0:
+                #if we're at ply 0, we need to call our eval function & return
+                return (self.score(board), m)
+            if board.gameOver():
+                return (-1, -1)  # Can't make a move, the game is over
+            nb = deepcopy(board)
+            #make a new board
+            nb.makeMove(self, m)
+            #try the move
+            opp = Player(self.opp, self.type, self.ply)
+            s = opp.minValue(nb, ply-1, turn)
+            #and see what the opponent would do next
+            if s > score:
+                #if the result is better than our best score so far, save that move,score
+                move = m
+                score = s
+        #return the best score and move so far
+        return score, move
+
+    def maxValue(self, board, ply, turn):
+        """ Find the minimax value for the next move for this player
+        at a given board configuation. Returns score."""
+        if board.gameOver():
+            return turn.score(board)
+        score = -INFINITY
+        for m in board.legalMoves(self):
+            if ply == 0:
+                #print "turn.score(board) in max value is: " + str(turn.score(board))
+                return turn.score(board)
+            # make a new player to play the other side
+            opponent = Player(self.opp, self.type, self.ply)
+            # Copy the board so that we don't ruin it
+            nextBoard = deepcopy(board)
+            nextBoard.makeMove(self, m)
+            s = opponent.minValue(nextBoard, ply-1, turn)
+            #print "s in maxValue is: " + str(s)
+            if s > score:
+                score = s
+        return score
+    
+    def minValue(self, board, ply, turn):
+        """ Find the minimax value for the next move for this player
+            at a given board configuation. Returns score."""
+        if board.gameOver():
+            return turn.score(board)
+        score = INFINITY
+        for m in board.legalMoves(self):
+            if ply == 0:
+                #print "turn.score(board) in min Value is: " + str(turn.score(board))
+                return turn.score(board)
+            # make a new player to play the other side
+            opponent = Player(self.opp, self.type, self.ply)
+            # Copy the board so that we don't ruin it
+            nextBoard = deepcopy(board)
+            nextBoard.makeMove(self, m)
+            s = opponent.maxValue(nextBoard, ply-1, turn)
+            #print "s in minValue is: " + str(s)
+            if s < score:
+                score = s
+        return score
+
+
+    # The default player defines a very simple score function
+    # You will write the score function in the MancalaPlayer below
+    # to improve on this function.
+    def score(self, board):
+        """ Returns the score for this player given the state of the board """
+        if board.hasWon(self.num):
+            return 100.0
+        elif board.hasWon(self.opp):
+            return 0.0
+        else:
+            return 50.0
+
+    # You should not modify anything before this point.
+    # The code you will add to this file appears below this line.
+
+    # You will write this function (and any helpers you need)
+    # You should write the function here in its simplest form:
+    #   1. Use ply to determine when to stop (when ply == 0)
+    #   2. Search the moves in the order they are returned from the board's
+    #       legalMoves function.
+    # However, for your custom player, you may copy this function
+    # and modify it so that it uses a different termination condition
+    # and/or a different move search order.
+    def alphaBetaMove(self, board, ply):
+        """ Choose a move with alpha beta pruning.  Returns (score, move) """
+        
+        move = -1
+        alpha = -INFINITY
+        beta = INFINITY
+        turn = self
+        for m in board.legalMoves(self):
+            #for each legal move
+            if ply == 0:
+                #if we're at ply 0, we need to call our eval function & return
+                return (self.score(board), m)
+            if board.gameOver():
+                return (-1, -1)  # Can't make a move, the game is over
+            nb = deepcopy(board)
+            #make a new boards
+            nb.makeMove(self, m)
+            #try the move
+            opp = Player(self.opp, self.type, self.ply)
+            s = opp.abMinValue(alpha, beta, nb, ply-1, turn)
+            #and see what the opponent would do next
+            if s > alpha:
+                #if the result is better than our best score so far, save that move,score
+                move = m
+                alpha = s
+            #if s < beta:
+             #   beta = s
+                # this may not have an effect but it may make the algorithm more efficient
+        #return the best score and move so far
+        return alpha, move
+
+    def abMinValue(self, alpha, beta, nb, ply, turn):
+        """find the best utility value using alpha-beta pruning. Returns score"""
+        if nb.gameOver():
+            return turn.score(nb)
+        #score = INFINITY
+        for m in nb.legalMoves(self):
+            if ply == 0:
+                #print "turn.score(board) in min Value is: " + str(turn.score(board))
+                return turn.score(nb)
+            # make a new player to play the other side
+            opponent = Player(self.opp, self.type, self.ply)
+            # Copy the board so that we don't ruin it
+            nextBoard = deepcopy(nb)
+            nextBoard.makeMove(self, m)
+            s = opponent.abMaxValue(alpha, beta, nextBoard, ply-1, turn)
+            #print "s in minValue is: " + str(s)
+            if s < alpha:
+                return s
+            if s < beta:
+                beta = s
+        return beta
+                
+    def abMaxValue(self, alpha, beta, nb, ply, turn):
+        """ Find the minimax value for the next move for this player
+        at a given board configuation. Returns score."""
+        if nb.gameOver():
+            return turn.score(nb)
+        #score = -INFINITY
+        for m in nb.legalMoves(self):
+            if ply == 0:
+                #print "turn.score(board) in max value is: " + str(turn.score(board))
+                return turn.score(nb)
+            # make a new player to play the other side
+            opponent = Player(self.opp, self.type, self.ply)
+            # Copy the board so that we don't ruin it
+            nextBoard = deepcopy(nb)
+            nextBoard.makeMove(self, m)
+            s = opponent.abMinValue(alpha, beta, nextBoard, ply-1, turn)
+            #print "s in maxValue is: " + str(s)
+            if s > beta:
+                return s
+            if s > alpha:
+                alpha = s
+        return alpha
+
+    def chooseMove(self, board):
+        """ Returns the next move that this player wants to make """
+        if self.type == self.HUMAN:
+            move = input("Please enter your move:")
+            while not board.legalMove(self, move):
+                print move, "is not valid"
+                move = input( "Please enter your move" )
+            return move
+        elif self.type == self.RANDOM:
+            move = choice(board.legalMoves(self))
+            print "chose move", move
+            return move
+        elif self.type == self.MINIMAX:
+            val, move = self.minimaxMove(board, self.ply)
+            print "chose move", move, " with value", val
+            return move
+        elif self.type == self.ABPRUNE:
+            val, move = self.alphaBetaMove(board, self.ply)
+            print "chose move", move, " with value", val
+            return move
+        elif self.type == self.CUSTOM:
+            # TODO: Implement a custom player
+            # You should fill this in with a call to your best move choosing
+            # function.  You may use whatever search algorithm and scoring
+            # algorithm you like.  Remember that your player must make
+            # each move in about 10 seconds or less.
+            print "Custom player not yet implemented"
+            return -1
+        else:
+            print "Unknown player type"
+            return -1
+
+
+# Note, you should change the name of this player to be your netid
+class MancalaPlayer(Player):
+    """ Defines a player that knows how to evaluate a Mancala gameboard
+        intelligently """
+
+    def score(self, board):
+        """ Evaluate the Mancala board for this player """
+        if board.hasWon(self.num):
+            return 100.0
+        elif board.hasWon(self.opp):
+            return 0.0
+        else:
+            zero_d_factor = 2.0
+            next_d_factor = 1.0
+            self_score = 0.0
+            # intialize the score to return
+
+            self_cups = board.getPlayersCups(self.num)  
+            opp_cups = board.getPlayersCups(self.opp)
+            # get the player cups
+            self_mancala = board.scoreCups[self.num-1]
+            opp_mancala = board.scoreCups[self.opp-1]
+            # get the number of stones in the mancala boards
+
+            self_score = zero_d_factor*(self_mancala - opp_mancala)
+            # account pieces in mancala
+            for k,v in enumerate(self_cups):
+                if (k+v)%6 is 0 and v < 6:
+                    self_score += zero_d_factor
+                elif (k+v+1)%6 is 0 and v < 6:
+                    self_score += next_d_factor
+                # account for moves that can land pieces in mancala 
+
+                if v is 0 and opp_cups[k] is not 0:
+                    self_score += zero_d_factor
+                elif  v is 0:
+                    self_score += next_d_factor
+
+            return self_score
+        
